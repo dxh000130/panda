998d88e696d12759c2b48af763b8aed5ddaa6359
Jason Young <jyoung8607@gmail.com>
2021-07-18 13:05:28 -0400

    testing quick and dirty filter for GM

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -209,10 +209,21 @@ static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   return tx;
 }
 
+static int gm_usb_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_filter) {
+  UNUSED(bus_num);
+  int addr = GET_ADDR(to_filter);
+  int usb_push = false;
+
+  usb_push |= !((addr == 0xc0) || (addr == 0xc1) || (addr == 0xc5) || (addr == 0xc7) || (addr == 0xd3) || \
+      (addr == 0xf9) || (addr == 0x18e) || (addr == 0x1c7) || (addr == 0x1ce) || (addr == 0x321) || (addr == 0x130) || \
+      (addr == 0x140) || (addr == 0x160) || (addr == 0x1e5));
+
+  return usb_push;
+}
 
 const safety_hooks gm_hooks = {
   .init = nooutput_init,
-  .usb = default_usb_hook,
+  .usb = gm_usb_hook,
   .rx = gm_rx_hook,
   .tx = gm_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
