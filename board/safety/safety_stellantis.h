22c923d2cd8d509b683653a1224b835f330804e4
Jason Young <jyoung8607@gmail.com>
2021-09-12 21:16:40 -0400

    initial draft / testing in progress

--- board/safety/safety_stellantis.h
+++ board/safety/safety_stellantis.h
@@ -1,19 +1,32 @@
+// TODO: haven't validated these numbers, also need to comply with comma safety standards
 const int STELLANTIS_MAX_STEER = 261;
-const int STELLANTIS_MAX_RT_DELTA = 112;        // max delta torque allowed for real time checks
-const uint32_t STELLANTIS_RT_INTERVAL = 250000;  // 250ms between real time checks
-const int STELLANTIS_MAX_RATE_UP = 10;
+const int STELLANTIS_MAX_RT_DELTA = 112;            // TBD
+const uint32_t STELLANTIS_RT_INTERVAL = 250000;     // 250ms between real time checks
+const int STELLANTIS_MAX_RATE_UP = 5;
 const int STELLANTIS_MAX_RATE_DOWN = 50;
-const int STELLANTIS_MAX_TORQUE_ERROR = 240;    // max torque cmd in excess of torque motor up from 240
-const int STELLANTIS_GAS_THRSLD = 30;  // 7% more than 2m/s
-const int STELLANTIS_STANDSTILL_THRSLD = 10;  // about 1m/s
-const CanMsg STELLANTIS_TX_MSGS[] = {{166, 0, 8}, {250, 0, 8}}; // {177, 0, 8}};  // 177 is for long
+// TODO: why do we need gas/standstill thresholds? autoresume spam not working yet maybe?
+const int STELLANTIS_GAS_THRSLD = 30;               // 7% more than 2m/s
+const int STELLANTIS_STANDSTILL_THRSLD = 10;        // about 1m/s
+
+// Safety-relevant CAN messages for the Stellantis 5th gen RAM (DT) platform
+#define MSG_EPS_1           0x23  // EPS steering angle and assist motor torque
+#define MSG_EPS_2           0x49  // EPS driver input torque and angle-change rate
+#define MSG_ABS_1           0x79  // Brake pedal and pressure
+#define MSG_TPS_1           0x81  // Throttle position sensor
+#define MSG_WHEEL_SPEEDS    0x8B  // ABS wheel speeds
+#define MSG_DASM_ACC        0x99  // ACC engagement states from DASM
+#define MSG_DASM_LKAS       0xA6  // LKAS controls from DASM
+#define MSG_ACC_BUTTONS     0xB1  // Cruise control buttons
+#define MSG_DASM_HUD        0xFA  // LKAS HUD and auto headlight control from DASM
+
+const CanMsg STELLANTIS_TX_MSGS[] = {{MSG_DASM_LKAS, 0, 8}, {MSG_DASM_HUD, 0, 8}, {MSG_ACC_BUTTONS, 2, 8}};
 
 AddrCheckStruct stellantis_addr_checks[] = {
-  {.msg = {{35, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 10000U}, { 0 }, { 0 }}},  // EPS module
-  {.msg = {{139, 0, 8, .check_checksum = false, .max_counter = 0U, .expected_timestep = 20000U}, { 0 }, { 0 }}},  // wheel speeds
-  {.msg = {{153, 0, 8, .check_checksum = false, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},  // forward cam ACC
-  {.msg = {{129, 0, 8, .check_checksum = false, .max_counter = 15U,  .expected_timestep = 20000U}, { 0 }, { 0 }}},  // gas pedal
-  {.msg = {{121, 0, 8, .check_checksum = false, .max_counter = 15U,  .expected_timestep = 20000U}, { 0 }, { 0 }}},  // brake pressed
+  {.msg = {{MSG_EPS_1, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_ABS_1, 0, 8, .check_checksum = false, .max_counter = 15U,  .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_TPS_1, 0, 8, .check_checksum = false, .max_counter = 15U,  .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_WHEEL_SPEEDS, 0, 8, .check_checksum = false, .max_counter = 0U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_FWD_CAM_ACC, 0, 8, .check_checksum = false, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
 };
 #define STELLANTIS_ADDR_CHECK_LEN (sizeof(stellantis_addr_checks) / sizeof(stellantis_addr_checks[0]))
 addr_checks stellantis_rx_checks = {stellantis_addr_checks, STELLANTIS_ADDR_CHECK_LEN};
@@ -23,6 +36,7 @@ static uint8_t stellantis_get_checksum(CAN_FIFOMailBox_TypeDef *to_push) {
   return (uint8_t)(GET_BYTE(to_push, checksum_byte));
 }
 
+// TODO: centralize/combine with Chrysler?
 static uint8_t stellantis_compute_checksum(CAN_FIFOMailBox_TypeDef *to_push) {
   /* This function does not want the checksum byte in the input data.
   jeep ram canbus checksum from http://illmatics.com/Remote%20Car%20Hacking.pdf */
@@ -70,7 +84,7 @@ static int stellantis_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     int addr = GET_ADDR(to_push);
 
     // Measured eps torque
-    if (addr == 35) {
+    if (addr == MSG_EPS_2) {
       int torque_meas_new = ((GET_BYTE(to_push, 4) & 0x7U) << 8) + GET_BYTE(to_push, 5) - 1024U;
 
       // update array of samples
@@ -78,7 +92,7 @@ static int stellantis_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     // enter controls on rising edge of ACC, exit controls on ACC off
-    if (addr == 153) {
+    if (addr == MSG_DASM_ACC) {
       int cruise_engaged = ((GET_BYTE(to_push, 2) & 0x38) >> 3) == 7;
       if (cruise_engaged && !cruise_engaged_prev) {
         controls_allowed = 1;
@@ -90,7 +104,7 @@ static int stellantis_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     // update speed
-    if (addr == 139) {
+    if (addr == MSG_WHEEL_SPEEDS) {
       int speed_l = (GET_BYTE(to_push, 0) << 4) + (GET_BYTE(to_push, 1) >> 4);
       int speed_r = (GET_BYTE(to_push, 2) << 4) + (GET_BYTE(to_push, 3) >> 4);
       vehicle_speed = (speed_l + speed_r) / 2;
@@ -98,12 +112,12 @@ static int stellantis_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     // exit controls on rising edge of gas press
-    if (addr == 129) {
+    if (addr == MSG_TPS_1) {
       gas_pressed = ((GET_BYTE(to_push, 5) & 0x7F) != 0) && ((int)vehicle_speed > STELLANTIS_GAS_THRSLD);
     }
 
     // exit controls on rising edge of brake press
-    if (addr == 121) {
+    if (addr == MSG_ABS_1) {
       brake_pressed = (GET_BYTE(to_push, 0) & 0x7) == 5;
       if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
         controls_allowed = 0;
@@ -111,7 +125,7 @@ static int stellantis_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
       brake_pressed_prev = brake_pressed;
     }
 
-    generic_rx_checks((addr == 0xa6));
+    generic_rx_checks((addr == MSG_DASM_LKAS));
   }
   return valid;
 }
@@ -141,8 +155,9 @@ static int stellantis_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
       violation |= max_limit_check(desired_torque, STELLANTIS_MAX_STEER, -STELLANTIS_MAX_STEER);
 
       // *** torque rate limit check ***
-      violation |= dist_to_meas_check(desired_torque, desired_torque_last,
-        &torque_meas, STELLANTIS_MAX_RATE_UP, STELLANTIS_MAX_RATE_DOWN, STELLANTIS_MAX_TORQUE_ERROR);
+      violation |= driver_limit_check(desired_torque, desired_torque_last, &torque_driver,
+        STELLANTIS_MAX_STEER, STELLANTIS_MAX_RATE_UP, STELLANTIS_MAX_RATE_DOWN,
+        STELLANTIS_DRIVER_TORQUE_ALLOWANCE, STELLANTIS_DRIVER_TORQUE_FACTOR);
 
       // used next time
       desired_torque_last = desired_torque;
@@ -176,7 +191,7 @@ static int stellantis_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   }
 
   // FORCE CANCEL: only the cancel button press is allowed
-  if (addr == 177) {
+  if (addr == MSG_ACC_BUTTONS) {
     if ((GET_BYTE(to_send, 0) != 1) || ((GET_BYTE(to_send, 1) & 1) == 1)) {
       tx = 1;
     }
@@ -191,12 +206,12 @@ static int stellantis_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   int addr = GET_ADDR(to_fwd);
 
   if (!relay_malfunction) {
-    // forward CAN 0 -> 2 so stock LKAS camera sees messages
+    // forward all messages from the rest of the car toward DASM
     if (bus_num == 0) {
       bus_fwd = 2;
     }
-    // forward all messages from camera except LKAS_COMMAND and LKAS_HUD
-    if ((bus_num == 2) && (addr != 166)) { // && (addr != 250)) { - restoring auto high beams for now
+    // selectively forward messages from DASM toward the rest of the car
+    if ((bus_num == 2) && (addr != MSG_DASM_LKAS) && (addr != MSG_DASM_HUD)) {
       bus_fwd = 0;
     }
   }
@@ -204,7 +219,7 @@ static int stellantis_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 }
 
 static const addr_checks* stellantis_init(int16_t param) {
-  UNUSED(param);
+  UNUSED(param);  // TODO: may need a parameter to choose steering angle signal width/precision for 2019 vs 2021 RAM
   controls_allowed = false;
   relay_malfunction_reset();
   return &stellantis_rx_checks;
